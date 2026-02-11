c1362a1941b2d395ff95f98aa1749cd40623cd97
Edu4r2 <tiendaonline3d@gmail.com>
2026-02-10 21:32:40 -0500

    chore: Update dependencies and regenerate Vite optimized modules.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        obfuscatorPlugin({
+            options: {
+                compact: true,
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 0.5,
+                deadCodeInjection: true,
+                deadCodeInjectionThreshold: 0.3,
+                identifierNamesGenerator: 'hexadecimal',
+                renameGlobals: false,
+                selfDefending: true,
+                stringArray: true,
+                stringArrayEncoding: ['base64'],
+                stringArrayThreshold: 0.75,
+                splitStrings: true,
+                splitStringsChunkLength: 10,
+            },
+        }),
+    ],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+    },
 })
