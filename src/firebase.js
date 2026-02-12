35039ac6f261c1872815504a69eece5084fac906
Edu4r2 <128761369+Edu4r2@users.noreply.github.com>
2026-02-12 18:40:40 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,3 @@
 
-import { initializeApp } from "firebase/app";
-const firebaseConfig = {
-    apiKey: "AIzaSyAtbFGPfyjTHl-LnGQRx4jOWyi8e_YfxQQ",
-    authDomain: "dstudio-966a9.firebaseapp.com",
-    projectId: "dstudio-966a9",
-    storageBucket: "dstudio-966a9.firebasestorage.app",
-    messagingSenderId: "1043290894707",
-    appId: "1:1043290894707:web:be9299bf440275eac51506"
-};
-const app = initializeApp(firebaseConfig);
 
-export default app;
+
